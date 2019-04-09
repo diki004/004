878a45bf4276e5a558f64baf646d39c3c5c2c162
diki004 <49420447+diki004@users.noreply.github.com>
2019-04-09 08:16:45 +0700

    004

---
+++ 004.sh
@@ -0,0 +1,128 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Tools 004"
+sleep 1
+echo "\033[32;1m [+]ALAT PERANG 004"
+sleep 1
+echo "\033[32;1m [+]CREATED BY 004"
+sleep 1
+echo "\033[32;1m [+]CONTACT: 004haxor@gmail.com"
+sleep 1
+echo "\033[31;1m [+]Black Illusion Security X Silent Squad"
+sleep 1
+echo "\033[36;1m Daftar Tools :"
+echo "1. Spam Call Tokopedia"
+echo "2. Spam Sms Telkomsel"
+echo "3. Spam Sms Matahari Mall"
+echo "4. Spam Sms PHD"
+echo "5. Spam Sms Jd.Id"
+echo "6. Spam Email Bomber"
+echo "7. Spam Chat WhatsApp"
+echo "8. Tools Mbf v2"
+echo "0. Keluar"
+echo "\033[32;1m69. Informasi Tools Ini Tod"
+echo "\033[31;1m *Pilih Angkanya Gan >:("
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+figlet "Spam WhatsApp"
+echo "\033[32;1m"
+php 7.php
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+figlet "Mbf v2"
+echo "\033[31;1m"
+python2 8.py
+fi
+
+if
+[ $mrrm = 69 ] || [ $mrrm = 69 ]
+then
+clear
+toilet -f slant --gay "Tools 004"
+echo "\033[31;1mALAT PERANG HACKING"
+sleep 1
+echo "\033[33;1mRECODE BY 004"
+sleep 1
+echo "\033[32;1mTools Ini Masih Tahap Pengembangan:v"
+sleep 1
+echo "\033[36;1mTEAM: BLACK ILLUSION SECURITY X SILENT SQUAD"
+sleep 1
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Bl4ckMouZ & Mr.xasw:v"
+echo "ALL FRIENDS DEFACER"
+sleep 1
+echo "And All Member Indonesian Hacker"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh 004.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa Ya Ngentod :v :)"
+sleep 1
+fi
+
